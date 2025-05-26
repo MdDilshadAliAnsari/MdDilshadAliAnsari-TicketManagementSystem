414008aff389b787de9474c72a44a57493c33dbb
MdDilshadAliAnsari <104012788+MdDilshadAliAnsari@users.noreply.github.com>
2025-05-26 18:01:07 +0530

    1
    1

---
+++ TMS.Domain/Task.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Domain
+{
+    public class Tassk
+    { 
+        public int?                     TASKID { get; set; } 
+        public int?                     PROJECTID { get; set; }
+        public string?                  DESCRIPTION { get; set; }
+        public int?                     PRIORITY { get; set; }
+        public DateTime?                DUEDATE { get; set; } 
+        public int?                     USERID { get; set; }
+        public int?                     ISDELETED { get; set; }
+        public int?                     CREATEDBY { get; set; }
+        public DateTime?                CREATEDON { get; set; }
+        public int?                     UPDATEDBY { get; set; }
+        public DateTime? UPDATEDON { get; set; }
+    }
+
+ 
+
+}
---
+++ TMS.Domain/Tsecret.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Domain
+{ 
+    public  static class Tsecret
+    {  
+        public static readonly string ClientIdProd = "e98bf70a-9c3b-4c38-8580-4b7f936f1e6e";
+        public static readonly string TenantIdProd = "35d72887-7230-4bed-869f-58dce1433da2";
+        public static readonly string ClientSecretProd = "bA~8Q~iMNtwySZJ9jh.41nh-k2mu89GkHZRGLbUA";
+        public static readonly string UrlProd = "https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messages?$filter=isRead eq false";
+
+        public static readonly string ClientIdTest = "bf24f8a3-15c1-4a6c-acf7-e2ec22325ea8";
+        public static readonly string TenantIdTest = "35d72887-7230-4bed-869f-58dce1433da2";
+        public static readonly string ClientSecretTest = "jst8Q~1PaAtq44.X3P9quEEVEoGOfJRMLHOSGaf-";
+        public static readonly string UrlTest = "https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messages?$filter=isRead eq false";
+
+
+
+    }
+}
